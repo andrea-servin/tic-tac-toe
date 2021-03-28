bb77c755f833913e3c29c5926788b94ab446321c
andrea-servin <servinmendoza.andrea@gmail.com>
2021-03-27 23:50:02 -0400

    Scaffolding components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Game from '../components/Game'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1>TIC-TAC-TOE</h1>
+        <Game />
       </main>
 
       <footer className={styles.footer}>
