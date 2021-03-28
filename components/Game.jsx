4d39ff755f361cca1413cde4f2301b033f942dc3
andrea-servin <servinmendoza.andrea@gmail.com>
2021-03-28 00:55:14 -0400

    Added handleClick and renderMoves function

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,27 +1,40 @@
 import React, { useState } from 'react'
 import calculateWinner from '../utils/calculateWinner'
 import Board from './Board'
-
+const style = {
+    width: "200px",
+    margin: "20px auto"
+}
 const Game = () => {
 
     const [board, setboard] = useState(Array(9).fill(null))
     const [nextX, setnextX] = useState(true)
     const winner = calculateWinner(board)
 
-    const handleClick = () => {
-
+    const handleClick = (i) => {
+        const boardCopy = [...board]
+        if (winner || boardCopy[i]) return
+        boardCopy[i] = nextX ? 'X' : 'O'
+        setboard(boardCopy)
+        setnextX(!nextX)
     }
 
     const jumpTo = () => {
-        
+
     }
 
     const renderMoves = () => {
-        
+        return <button style={{ width: "80px", height: "30px", background: "lightblue", color: "blue", outline: "none" }} onClick={() => setboard(Array(9).fill(null))}>Start Game</button>
     }
 
     return (
+        <>
             <Board squares={board} onClick={handleClick} />
+            <div style={style}>
+                <p>{winner ? "Winner: " + winner : "Next Player: " + (nextX ? "X" : "O")}</p>
+                {renderMoves()}
+            </div>
+        </>
     )
 }
 
